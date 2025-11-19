804ee559cd7a9ae9f93ff94c0660ad2c9422c11c
nitishchahal <nitishchoudharyc2@gmail.com>
2025-11-20 01:46:28 +0530

    n

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -99,7 +99,7 @@ const Footer = () => {
               <p className="text-lg font-semibold text-[#1D3557] mb-3">Contact</p>
               <ul className="space-y-2 text-sm text-[#457B9D]">
                 <li>+91 600-655-6223</li>
-                <li>support@Dr_Authority.com</li>
+                <li>dr.authority@gmail.com</li>
                 <li className="text-[#1D3557]/70 text-xs">
                   24/7 online appointment support
                 </li>
